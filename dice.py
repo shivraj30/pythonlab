69598f0ae82190505c5f6dd09482d1ce0227684e
shivraj30 <48012826+shivraj30@users.noreply.github.com>
2019-03-05 15:04:01 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,9 @@
+import random
+while True:
+	r = input("press r to roll, q to quit")
+
+	if r == 'r':
+		print(random.randint(1,6))
+	elif r == 'q':
+		print("bye!")
+		exit()
---
+++ shivraj.py
@@ -0,0 +1,18 @@
+def func1(m):
+    print("hii",m)
+func1("man")
+
+def func(a,b,c):
+	d=a+b+c
+	print(a,b,c,d)
+func(3,5,8)
+
+def func2(a,b):
+	c=a+b
+	return c
+
+def func3():
+	print("hello,i am going to call the function")
+	s=func2(2,7)
+	print("''addition is",s)
+func3()
